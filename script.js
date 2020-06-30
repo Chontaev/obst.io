9b086f6485b1468fff25bdb5699a546f242f932a
Chontaev <65342023+Chontaev@users.noreply.github.com>
2020-07-01 01:57:36 +0600

    Update script.js

--- script.js
+++ script.js
@@ -42,13 +42,13 @@ $(document).ready(function(){
 			{
 				breakpoint: 768,
 				settings: {
-					slidesToShow:2
+					slidesToShow:3
 				}
 			},
 			{
 				breakpoint: 550,
 				settings: {
-					slidesToShow:1
+					slidesToShow:3
 				}
 			}
 		]
